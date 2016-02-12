aff9a6a6f39f2460af619eabe0cbb7ddc7a65a01
Sam Hudson <me@samhudson.me>
2016-02-11 17:41:11 -0800

    DB change

--- config.ini.php
+++ config.ini.php
@@ -1,13 +1,13 @@
 
 <?php
 db_driver=mysql
-db_user=root
-db_password=root
+db_user=cl18-cs
+db_password=U/r26TwNC
 
 [dsn]
-host=localhost
+host=79.170.40.177
 port=3306
-dbname=our_service
+dbname=cl18-cs
 
 [db_options]
 PDO::MYSQL_ATTR_INIT_COMMAND=set names utf8
